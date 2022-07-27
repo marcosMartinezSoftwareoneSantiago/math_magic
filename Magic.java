80982c055da334e1c589fde66fb6b73b1d4a4447
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2022-07-27 15:23:28 +0200

    first commit

---
+++ Magic.java
@@ -0,0 +1,24 @@
+public class Magic {
+	public static void main(String[] args) {
+    //myNumber will be the original number
+    int myNumber = 136;
+    
+    int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne - myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
+    
+    System.out.println(stepSix);
+
+    int magicNumber = myNumber * myNumber;
+    magicNumber -= myNumber;
+    magicNumber /= myNumber;
+    magicNumber += 17;
+    magicNumber -= myNumber;
+    magicNumber /= 6;
+
+    System.out.println(magicNumber);
+	}
+}
